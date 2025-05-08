45544bf5fb8cf67ecb9ee1b34f40933a73e83ccc
Mario Dek <ninodek96@gmail.com>
2025-05-08 22:14:20 +0000

    quiero cambiar el icono que esta al lado de inventarit

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -5,7 +5,7 @@ import type React from 'react';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { Package, Users, Settings, LayoutDashboard, ScanBarcode, KeyRound, ShoppingCart } from 'lucide-react'; 
+import { Package, Users, Settings, LayoutDashboard, ScanBarcode, KeyRound, ShoppingCart, PackageSearch } from 'lucide-react'; 
 import { cn } from '@/lib/utils';
 import {
   SidebarContent,
@@ -37,9 +37,7 @@ export function AppSidebar() {
     <>
       <SidebarHeader>
         <div className="flex items-center gap-2 p-2">
-           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-primary">
-            <path fillRule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3A5.25 5.25 0 0 0 12 1.5Zm-3.75 5.25a3.75 3.75 0 0 0 7.5 0v3h-7.5v-3Zm-3.75 3V18a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5v-8.25a.75.75 0 0 0-.75-.75h-12a.75.75 0 0 0-.75.75Zm1.5 1.5a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.023 3.023a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1-.75-.75Zm-.023 3a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
-          </svg>
+           <PackageSearch className="w-6 h-6 text-primary" />
           {hasMounted && state === 'expanded' && <span className="font-semibold text-lg">INVENTARIT</span>}
         </div>
       </SidebarHeader>
@@ -155,3 +153,4 @@ export function AppSidebar() {
     </>
   );
 }
+
