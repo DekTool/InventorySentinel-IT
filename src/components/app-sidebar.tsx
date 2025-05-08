9098b2defa7bf2bd13e44e2ea5e681b387e1d9d4
Mario Dek <ninodek96@gmail.com>
2025-05-08 21:35:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <AppSidebar>
    
    with the text content:
    
        CAMBIAR LOGO POR EL DE SOLARIA ENERGIA Y MEDIO AMBIENTE
    
    Relevant files:
    
        - https://6000-idx-studio-1746385500443.cluster-6frnii43o5blcu522sivebzpii.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    INVENTARIT

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -40,7 +40,7 @@ export function AppSidebar() {
            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-primary">
             <path fillRule="evenodd" d="M12 1.5a5.25 5.25 0 0 0-5.25 5.25v3a3 3 0 0 0-3 3v6.75a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3v-6.75a3 3 0 0 0-3-3v-3A5.25 5.25 0 0 0 12 1.5Zm-3.75 5.25a3.75 3.75 0 0 0 7.5 0v3h-7.5v-3Zm-3.75 3V18a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5v-8.25a.75.75 0 0 0-.75-.75h-12a.75.75 0 0 0-.75.75Zm1.5 1.5a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.023 3.023a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1-.75-.75Zm-.023 3a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
           </svg>
-          {hasMounted && state === 'expanded' && <span className="font-semibold text-lg">CAMBIAR LOGO POR EL DE SOLARIA ENERGIA Y MEDIO AMBIENTE</span>}
+          {hasMounted && state === 'expanded' && <span className="font-semibold text-lg">INVENTARIT</span>}
         </div>
       </SidebarHeader>
       <SidebarContent className="flex-1 overflow-y-auto">
